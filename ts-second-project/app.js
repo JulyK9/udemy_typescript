cf4a0bf95f75c12caab720779fa0f50d1f9746a3
JKKIM <jkesonjk@gmail.com>
2023-01-30 12:46:37 +0900

    tuple enum 타입 확인 및 실습

--- ts-second-project/app.js
+++ ts-second-project/app.js
@@ -1,17 +1,55 @@
-var add = function (n1, n2, showResult, resultPhrase) {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("Incorrect input!");
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(resultPhrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Bruno",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user']
+// 전역 상수를 정희하고 관리: 휴먼에러 최소화
+var ADMIN = 0; // 문자 대신 숫자를 사용하면 코드의 양과 메모리 점유를 줄일 수 있음!
+var READ_ONLY = 1;
+var AUTHOR = 2;
+// 위의 상수화 관리를 enum으로 더 효과적으로 관리할 수 있음
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {})); // 자동으로 숫자가 할당됨, 대어보면 확인됨
+var person = {
+    name: "Bruno",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    // role: "READ ONLY USER",
+    // role: ADMIN,
+    role: Role.AUTHOR
 };
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is: ";
-var result = add(number1, number2, printResult, resultPhrase);
+var favoriteActivities;
+// let favoriteActivities: any[];
+// favoriteActivities = "Sports";
+// favoriteActivities = ["Sports", 1];
+favoriteActivities = ["Sports"];
+// console.log(person.nickname);
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) // Error
+}
+// if (person.role === "READ-ONLY-USER") {
+// if (person.role === ADMIN) {
+if (person.role === Role.AUTHOR) {
+    // console.log("is admin");
+    console.log("is author");
+}
--- ts-second-project/app.ts
+++ ts-second-project/app.ts
@@ -1,23 +1,61 @@
-const add = (
-  n1: number,
-  n2: number,
-  showResult: boolean,
-  resultPhrase: string
-) => {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect input!");
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(resultPhrase + result);
-  } else {
-    return n1 + n2;
-  }
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Bruno",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, 'admin', 'user']
+
+// 전역 상수를 정희하고 관리: 휴먼에러 최소화
+const ADMIN = 0; // 문자 대신 숫자를 사용하면 코드의 양과 메모리 점유를 줄일 수 있음!
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+// 위의 상수화 관리를 enum으로 더 효과적으로 관리할 수 있음
+enum Role {
+  ADMIN = 5, // 식별자인 숫자를 커스텀하게 넘버링, 레터링할 수 있음
+  READ_ONLY = 100,
+  AUTHOR = "AUTHOR",
+} // 자동으로 숫자가 할당됨, 대어보면 확인됨
+
+const person = {
+  name: "Bruno",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  // role: "READ ONLY USER",
+  // role: ADMIN,
+  role: Role.AUTHOR,
 };
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+let favoriteActivities: string[];
+// let favoriteActivities: any[];
+// favoriteActivities = "Sports";
+// favoriteActivities = ["Sports", 1];
+favoriteActivities = ["Sports"];
 
-const result = add(number1, number2, printResult, resultPhrase);
+// console.log(person.nickname);
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()) // Error
+}
+
+// if (person.role === "READ-ONLY-USER") {
+// if (person.role === ADMIN) {
+if (person.role === Role.AUTHOR) {
+  // console.log("is admin");
+  console.log("is author");
+}
