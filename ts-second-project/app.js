53bebcf331445c7f41f470bebf89f10b0d6effaa
JKKIM <jkesonjk@gmail.com>
2023-01-30 21:54:04 +0900

    literal 타입 및 union 타입과 조합 사용

--- ts-second-project/app.js
+++ ts-second-project/app.js
@@ -1,19 +1,27 @@
 // function combine(input1: number, input2: number) {
 // function combine(input1: string, input2: string) {
-function combine(input1, input2) {
+function combine(input1, input2, resultType) {
     // let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultType === "as-number") {
         // result = input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
-        return input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
+        return +input1 + +input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
         // return input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
     }
+    // else if (resultType === "as-number") {
+    //   return +input1 + +input2;
+    // }
     else {
         // result = input1.toString() + input2.toString();
         return input1.toString() + input2.toString();
     }
     // return result;
 }
-var combinedAges = combine(30, 20);
+var combinedAges = combine(30, 20, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("Max", "Bruno");
+var combinedStringAges = combine("30", "20", "as-number");
+console.log(combinedStringAges);
+// const combinedNames = combine("Max", "Bruno");
+// console.log(combinedNames);
+var combinedNames = combine("Max", "Bruno", "as-text");
 console.log(combinedNames);
--- ts-second-project/app.ts
+++ ts-second-project/app.ts
@@ -1,20 +1,38 @@
 // function combine(input1: number, input2: number) {
 // function combine(input1: string, input2: string) {
-function combine(input1: string | number, input2: string | number) {
+function combine(
+  input1: string | number,
+  input2: string | number,
+  // resultType: string
+  resultType: "as-number" | "as-text" // 유니언 타입과 리터럴 타입의 결합 사용
+) {
   // let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultType === "as-number"
+  ) {
     // result = input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
-    return input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
+    return +input1 + +input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
     // return input1 + input2; // string 이나 number 이외의 타입이 올 수도 있어서 에러가 나는 것
-  } else {
+  }
+  // else if (resultType === "as-number") {
+  //   return +input1 + +input2;
+  // }
+  else {
     // result = input1.toString() + input2.toString();
     return input1.toString() + input2.toString();
   }
   // return result;
 }
 
-const combinedAges = combine(30, 20);
+const combinedAges = combine(30, 20, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Bruno");
+const combinedStringAges = combine("30", "20", "as-number");
+console.log(combinedStringAges);
+
+// const combinedNames = combine("Max", "Bruno");
+// console.log(combinedNames);
+
+const combinedNames = combine("Max", "Bruno", "as-text");
 console.log(combinedNames);
