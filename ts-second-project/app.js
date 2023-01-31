2a87740a6dddd265f09d5ebca6c1b2932d399336
JKKIM <jkesonjk@gmail.com>
2023-01-31 23:06:13 +0900

    never 타입

--- ts-second-project/app.js
+++ ts-second-project/app.js
@@ -1,34 +1,15 @@
-function add2(n1, n2) {
-    return n1 + n2;
-    // return n1.toString() + n2.toString();
+var userInput;
+// let userInput; // 이것들과는 다름
+// let userInput: any; // 이것들과는 다름
+var userName;
+userInput = 5;
+userInput = "Max";
+// userName = userInput; // 에러
+if (typeof userInput === "string") {
+    userName = userInput; // 타입 체크로 타입 제한을 해줘야 에러 안남
 }
-// void 반환 타입
-// function printResult2(num: number) { // 추론방식(더 나음)
-function printResult2(num) {
-    // 명시적 표시
-    console.log("Result: " + num);
+// 에러 객체를 생성하여 넘기는 유틸 함수
+function generateError(message, code) {
+    throw { errorMessage: message, errorCode: code };
 }
-// 주의 undefined를 반환한다는 의미는 void 와는 다름
-function printResult3(num) {
-    // 명시적 표시
-    console.log("Result: " + num);
-    return; // 값을 반환하지 않는 반환문이 있는 것임. 따라서 함수는 undefined 타입임
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult2(add2(5, 12)); // Result: 17
-console.log(printResult2(add2(5, 12))); // undefined, 아무것도 반환하지 않는 함수의 반환값을 사용했기때문
-// let combineValues: Function;  // 인수가 2개인데도 허용해줘버리는 문제
-var combineValues;
-combineValues = add2;
-// combineValues = printResult2;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-// undefined는 타입스크립트에서 유효한 타입중 하나임
-// let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-    return result;
-});
+generateError("An error occured!", 500);
--- ts-second-project/app.ts
+++ ts-second-project/app.ts
@@ -6,8 +6,17 @@ let userName: string;
 userInput = 5;
 userInput = "Max";
 
-userName = userInput; // 에러
+// userName = userInput; // 에러
 
 if (typeof userInput === "string") {
   userName = userInput; // 타입 체크로 타입 제한을 해줘야 에러 안남
 }
+
+// 에러 객체를 생성하여 넘기는 유틸 함수
+function generateError(message: string, code: number): never {
+  throw { errorMessage: message, errorCode: code };
+}
+
+// generateError("An error occured!", 500);
+const result2 = generateError("An error occured!", 500);
+console.log(result);
