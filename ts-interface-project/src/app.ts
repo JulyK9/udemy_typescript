deb9ae05b6ddc96dd84ff2fd03715179db5574ea
JKKIM <jkesonjk@gmail.com>
2023-03-13 16:10:09 +0900

    feat: 인터페이스 확장

--- ts-interface-project/src/app.ts
+++ ts-interface-project/src/app.ts
@@ -1,13 +1,22 @@
+interface Named {
+  readonly name: string;
+}
+
 // 속성의 이름, 속성에 저장될 값의 타입이나 필드 정의를 추가
 // type Person = {
 // interface Person {
 // 인터페이스를 어떤 상수나 변수의 타입으로 사용하여 인터페이스 타입을 기반으로 하는 다른 타입의 클래스를 저장할 수 있음
 // 인터페이스 내에 readonly 제어자도 추가할 수 있지만 public, private 등은 지정할 수 없음
-interface Greetable {
+
+// 인터페이스를 여러개 확장할 수 있음(여러 인터페이스로부터 상속받을 수 있음)
+// interface Greetable {
+// interface Greetable extends Named, AnotherInterface {
+// 단, 클래스는 하나의 클래스로부터만 상속할 수 있고, 다수의 클래스로부터는 상속할 수 없음
+interface Greetable extends Named {
   // name: string = 'Bruno'; // 인터페이스는 초기값을 가질 수 없음 => 구조만 있을 뿐임
   // readonly 를 추가하여 인터페이스를 기반으로 구축하는 모든 객체의 속성이 한번만 설정되어야하며
   // 이후에는 읽기 전용으로 설정하여 객체가 초기화되면 변경할 수 없도록 할 수 있음
-  readonly name: string;
+  // readonly name: string;
   // age: number;
 
   // 메소드 추가
@@ -16,11 +25,16 @@ interface Greetable {
   greet(phrase: string): void;
 }
 
+// 인터페이스를 나누는 이유
+// 객체마다 필요하고 사용할 속성과 메소드가 다를 수 있기 때문
+// 어떤 객체에는 name을 입력하고 greed 메소드는 제외하는 한편
+// 다른 객체에는 greet과 name을 입력하고자 한다면 이런 식으로 나눌 수 있기 때문
+
 // Greetable 인터페이스를 사용해 여러 클래스 간에 공유할 수 있고
 // 이 인터페이스를 준수하는 모든 클래스가 name 클래스와 greet 메소드를 갖도록 하기 위해 클래스를 추가
 // 쉼표로 구분하여 여러 개의 인터페이스를 구현할 수 있다는 것이 상속과 차이점임
 // class Person implements Greetable, AnotherInterface {
-class Person implements Greetable {
+class Person implements Greetable, Named {
   name: string;
   age = 30;
 
